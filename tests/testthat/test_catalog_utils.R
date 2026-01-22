ad1f45738eb76220ff2426445880e9ca3a8f2a31
Paul Linza <zplinza@gmail.com>
2026-01-22 13:23:06 -0700

    Added tests for http and catalog_utils
    Added tests for http.R and catalog_utils.R

---
+++ tests/testthat/test_catalog_utils.R
@@ -0,0 +1,10 @@
+library(testthat)
+library(flowfabricr)
+
+# test auto_streamflow_params() for return type
+test_that("auto_streamflow_params returns a list", {
+  token <- get_bearer_token()
+  dataset_id <- "nws_owp_nwm_reanalysis_3_0"
+  result <- auto_streamflow_params(dataset_id)
+  expect_equal(typeof(result), "list")
+})
